6f2a293f165249b554d0cc2472d02604cc8d1c1c
sambanihit98 <sambanihit@gmail.com>
2025-09-05 18:41:11 +0800

    Create TrustProxies.php
    middleware

---
+++ app/Http/Middleware/TrustProxies.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
+use Illuminate\Http\Request;
+
+class TrustProxies extends Middleware
+{
+    /**
+     * The trusted proxies for this application.
+     *
+     * @var array|string|null
+     */
+    protected $proxies = '*'; // Trust all proxies (needed for Render)
+
+    /**
+     * The headers that should be used to detect proxies.
+     *
+     * @var int
+     */
+}
