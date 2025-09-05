f2d270e1aa1bfb558f965a6d32908488016fd614
sambanihit98 <sambanihit@gmail.com>
2025-09-06 02:35:26 +0800

    implement FilamentUser contract
    added FilamentUser contract to access Filament in non-local environments

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -2,13 +2,14 @@
 
 namespace App\Models;
 
+use Filament\Models\Contracts\FilamentUser;
 use Filament\Panel;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
 
-class Admin extends Authenticatable
+class Admin extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\AdminFactory> */
     use HasFactory;
--- app/Models/Employer.php
+++ app/Models/Employer.php
@@ -2,13 +2,14 @@
 
 namespace App\Models;
 
+use Filament\Models\Contracts\FilamentUser;
 use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
 
-class Employer extends Authenticatable
+class Employer extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\EmployerFactory> */
     use HasFactory;
