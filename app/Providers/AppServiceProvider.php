cc40524077affde9dc563650598dd8b75ba4a311
sambanihit98 <sambanihit@gmail.com>
2025-09-08 09:26:14 +0800

    Update AppServiceProvider.php
    got this solution from:
    https://github.com/livewire/livewire/discussions/3084

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,6 +16,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -27,6 +28,42 @@ class AppServiceProvider extends ServiceProvider
     {
         //not required if there's multiple panel/model
         //$this->app->bind(Authenticatable::class, Admin::class);
+
+        URL::macro(
+            'alternateHasCorrectSignature',
+            function (Request $request, $absolute = true, array $ignoreQuery = []) {
+                $ignoreQuery[] = 'signature';
+
+                $absoluteUrl = url($request->path());
+                $url = $absolute ? $absoluteUrl : '/' . $request->path();
+
+                $queryString = collect(explode('&', (string) $request
+                    ->server->get('QUERY_STRING')))
+                    ->reject(fn($parameter) => in_array(Str::before($parameter, '='), $ignoreQuery))
+                    ->join('&');
+
+                $original = rtrim($url . '?' . $queryString, '?');
+
+                // Use the application key as the HMAC key
+                $key = config('app.key'); // Ensure app.key is properly set in .env
+
+                if (empty($key)) {
+                    throw new \RuntimeException('Application key is not set.');
+                }
+
+                $signature = hash_hmac('sha256', $original, $key);
+                return hash_equals($signature, (string) $request->query('signature', ''));
+            }
+        );
+
+        URL::macro('alternateHasValidSignature', function (Request $request, $absolute = true, array $ignoreQuery = []) {
+            return URL::alternateHasCorrectSignature($request, $absolute, $ignoreQuery)
+                && URL::signatureHasNotExpired($request);
+        });
+
+        Request::macro('hasValidSignature', function ($absolute = true, array $ignoreQuery = []) {
+            return URL::alternateHasValidSignature($this, $absolute, $ignoreQuery);
+        });
     }
 
     /**
@@ -45,23 +82,13 @@ class AppServiceProvider extends ServiceProvider
         Route::aliasMiddleware('auth.multi', AuthPanelUser::class);
 
         //------------------------------------
-        if ($this->app->environment('production')) {
-            URL::forceScheme('https');
-
-            Request::setTrustedProxies(
-                ['*'], // trust all proxies (safe on Render)
-                Request::HEADER_X_FORWARDED_FOR |
-                    Request::HEADER_X_FORWARDED_HOST |
-                    Request::HEADER_X_FORWARDED_PORT |
-                    Request::HEADER_X_FORWARDED_PROTO
-            );
-        }
-
-        //------------------------------------
-
         // if (env('APP_ENV') === 'production') {
         //     URL::forceScheme('https');
         // }
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
+        //------------------------------------
     }
 
     protected $policies = [
